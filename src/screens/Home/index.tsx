ab1cbf4a00b87b0ed8c1fe1a14b71f2db7a4bed2
Ewerson Castelo Silva <ewerson.castelo@gmail.com>
2023-09-10 15:02:48 -0400

    chore: create struct default start

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,75 @@
-import { Text, View } from 'react-native';
+import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+import theme from '../../global/theme';
 import { styles } from './styles';
 
+import imageEmpty from '../../assets/images/empty-logo.png';
+import logoTodo from '../../assets/images/todo-logo.png';
+
 export function Home() {
-return (
-  <View style={styles.container}>
-    <Text style={styles.title}>Hello New App</Text>
-  </View>
-);
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          style={styles.logo}
+          source={logoTodo} 
+        />
+      </View>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Adicione uma nova tarefa tarefa"
+          placeholderTextColor={theme.colors.gray300} 
+        />
+
+        <TouchableOpacity
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.subHeader}>
+        <View style={styles.subHeaderLeft}>
+          <Text style={styles.tasksCreated}>
+            Criadas
+          </Text>
+
+          <View style={styles.tasksCreatedTotal}>
+            <Text style={styles.tasksCreatedText}>0</Text>
+          </View>
+        </View>
+
+
+        <View style={styles.subHeaderRight}>
+          <Text style={styles.tasksDone}>
+            Concluídas
+          </Text>
+          
+          <View style={styles.tasksDoneTotal}>
+            <Text style={styles.tasksDoneText}>
+              0
+            </Text>
+          </View>
+        </View>     
+      </View> 
+      
+      <View style={styles.content}>
+        <View style={styles.emptyTodo}>
+          <Image
+            style={styles.logoEmpty}
+            source={imageEmpty} 
+          />
+          <Text style={styles.textTitleEmpty}>
+            Você ainda não tem tarefas cadastradas
+          </Text>
+
+          <Text style={styles.textSubTitleEmpty}>
+            Crie tarefas e organize seus itens a fazer
+          </Text>
+        </View>
+      </View>
+    </View>
+  );  
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -3,17 +3,157 @@ import theme from "../../global/theme";
 
 export const styles = StyleSheet.create({
   container: {
-    backgroundColor: theme.colors.blueDark,
+    backgroundColor: theme.colors.gray600,
+    flex: 1,
+  },
+
+  header: {
+    backgroundColor: theme.colors.gray700,
+    height: 170,
     width: "100%",
-    height: "100%",
+
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  logo: {
+    width: 110.34,
+    height: 32,
+
+    marginTop: 24,
+  },
+
+  form: {
+    flexDirection: "row",
+    paddingHorizontal: 24,
+    marginTop: -25
+  },
+
+  input: {
+    backgroundColor: theme.colors.gray500,
+    height: 54,
+    padding: 16,
+    flex: 1,
+    marginRight: 4,
+
+    borderRadius: 8,
+  },
+
+  button: {
+    width: 52,
+    height: 52,
+
+    backgroundColor: theme.colors.blueDark,
+    borderRadius: 8,
 
     justifyContent: "center",
     alignItems: "center"
   },
 
-  title: {
-    color: '#FFFFFF',
-    fontSize: 18,
-    fontFamily: theme.fonts.regular
+  buttonText : {
+    color: "#FFF",
+    fontSize: 24
+  },
+
+  subHeader : {
+    height: 40, 
+    flexDirection: "row",
+
+    justifyContent: "space-between",
+    marginTop: 32,
+    marginHorizontal: 24,
+
+    borderBottomWidth: 1,
+    borderColor: theme.colors.gray400,
+    borderStyle: "solid"
+  },
+
+  subHeaderLeft : {
+    flexDirection: "row",
+  },
+
+  subHeaderRight : {
+    flexDirection: "row",
+  },
+
+  tasksCreated: {
+    color: theme.colors.blue,
+    fontSize: 14,
+
+    fontFamily: theme.fonts.bold,
+  },
+
+  tasksDone : {
+    color: theme.colors.purple,
+    fontSize: 14,
+
+    fontFamily: theme.fonts.bold,
+  },
+
+  tasksCreatedTotal : {
+    backgroundColor: theme.colors.gray400,
+    borderRadius: 12,
+
+    width: 29,
+    height: 19,
+
+    alignItems: "center",
+    justifyContent: "center",
+
+    marginLeft: 8
+  },
+
+  tasksDoneTotal : {
+    backgroundColor: theme.colors.gray400,
+    borderRadius: 12,
+
+    width: 29,
+    height: 19,
+
+    alignItems: "center",
+    justifyContent: "center",
+
+    marginLeft: 8
+  },
+
+  tasksCreatedText: {
+    color: theme.colors.gray200,
+    fontSize: 12,
+    fontFamily: theme.fonts.bold,
+  },
+
+  tasksDoneText: {
+    color: theme.colors.gray200,
+    fontSize: 12,
+    fontFamily: theme.fonts.bold,
+  },
+
+  content : {
+    flex: 1,
+
+    paddingHorizontal: 24,
+    paddingVertical: 48,
+  },
+
+  emptyTodo : {
+    alignItems: "center",
+  },
+
+  logoEmpty : {
+    width: 56,
+    height: 56
+  },
+
+  textTitleEmpty : {
+    color: theme.colors.gray300,
+    fontFamily: theme.fonts.bold,
+    fontSize: 14,
+
+    marginTop: 14
+  },
+
+  textSubTitleEmpty : {
+    color: theme.colors.gray300,
+    fontSize: 14,
   }
 });
